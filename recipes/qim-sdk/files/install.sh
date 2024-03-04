2fd67d20042755eb712e0a760ad576e825803863
Divya Theja K P <quic_divyathe@quicinc.com>
2024-03-03 16:34:23 -0800

    qimsdk-pkg: Amend bbclass
    - Remove unused variables
    - Do not clean the install scripts after recipe build
    - pack dependencies for gst plugins
    
    Change-Id: I7811af844b6d245be2200a8d41a1a7757082e6c4

--- recipes/qim-sdk/files/install.sh
+++ recipes/qim-sdk/files/install.sh
@@ -6,8 +6,7 @@
 SDK_NAME="QIM_SDK"
 
 FOUND_PKGS=""
-QIM_PKG_DIR="/opt/qcom/qimsdk/"
-PKG_LIST_FILE="/opt/qcom/qimsdk/$SDK_NAME.list"
+PKG_LIST_FILE="/opt/qcom/qimsdk/${SDK_NAME}.list"
 
 
 # check permission for execute this script
@@ -27,46 +26,34 @@ function scan_qim_packages() {
         | tr '\n' ' '`
 }
 
-function qim_sdk_env_file() {
-    echo "export PATH=${QIM_PKG_DIR}/usr/bin:\$PATH" > ${QIM_PKG_DIR}/qim-sdk.sh
-    echo "export LD_LIBRARY_PATH=${QIM_PKG_DIR}/usr/lib:\$LD_LIBRARY_PATH" >> ${QIM_PKG_DIR}/qim-sdk.sh
-    echo "export GST_PLUGIN_PATH=${QIM_PKG_DIR}/usr/lib/gstreamer-1.0:\$GST_PLUGIN_PATH" >> ${QIM_PKG_DIR}/qim-sdk.sh
-    echo "export LD_LIBRARY_PATH=${QIM_PKG_DIR}/lib:\$LD_LIBRARY_PATH" >> ${QIM_PKG_DIR}/qim-sdk.sh
-    echo "export GST_PLUGIN_SCANNER=${QIM_PKG_DIR}/usr/libexec/gstreamer-1.0/gst-plugin-scanner" >> ${QIM_PKG_DIR}/qim-sdk.sh
-
-    chmod +x ${QIM_PKG_DIR}/qim-sdk.sh
-}
-
 # install packages and save list to file
 function install_qim_packages() {
 
-    install_command="opkg install --force-reinstall --force-depends --force-overwrite -o ${QIM_PKG_DIR}"
+    install_command="opkg install --force-reinstall --force-depends --force-overwrite"
 
-    for PKG_FILE in $FOUND_PKGS; do
-        $install_command $PKG_FILE
+    for PKG_FILE in ${FOUND_PKGS}; do
+        ${install_command} ${PKG_FILE}
     done
 
-    if [ -f "$PKG_LIST_FILE" ]; then
-        rm -f "$PKG_LIST_FILE"
-    fi
+    rm -f "${PKG_LIST_FILE}"
 
-    for pkg in $FOUND_PKGS; do
-        pkg_name=`echo $pkg | awk -F'/' '{print $NF}' | awk -F'_' '{print $1}'`
-        echo $pkg_name >> $PKG_LIST_FILE
+    for pkg in ${FOUND_PKGS}; do
+        pkg_name=`echo ${pkg} | awk -F'/' '{print $NF}' | awk -F'_' '{print $1}'`
+        echo ${pkg_name} >> ${PKG_LIST_FILE}
     done
 }
 
 function main() {
 
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-    echo ">>> Install scripts for $SDK_NAME"
+    echo ">>> Install scripts for ${SDK_NAME}"
     echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
     echo
 
     check_permission
 
-    if [ -f $PKG_LIST_FILE ]; then
-        printf "WARN: $SDK_NAME has installed, "
+    if [ -f ${PKG_LIST_FILE} ]; then
+        printf "WARN: ${SDK_NAME} has installed, "
         while true; do
             read -p "Do you wish to install anyway? (Y/N)" yn
             case $yn in
@@ -79,20 +66,12 @@ function main() {
 
     scan_qim_packages
 
-    if [ ! -d "/opt/qcom/qimsdk/" ]; then
-        mkdir -p /opt/qcom/qimsdk
-    fi
-
-    if [ ! -d "$QIM_PKG_DIR" ]; then
-        mkdir -p "$QIM_PKG_DIR"
-    fi
+    mkdir -p /opt/qcom/qimsdk
 
     install_qim_packages
-    qim_sdk_env_file
 
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-    echo ">>> Installation done for $SDK_NAME at $QIM_PKG_DIR"
-    echo ">>> source $QIM_PKG_DIR/qim-sdk.sh before running usecases"
+    echo ">>> Installation done for ${SDK_NAME} at root"
     echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
     echo
 }
